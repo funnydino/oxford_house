332bf958d6d579a12706a2f9e3aa64b6316ef8f5
Aleksey Ponomarev <dazed39@mail.ru>
2020-11-24 22:27:52 +0200

    Some Little Bugs Fixes

--- js/recaptcha.js
+++
@@ -1,8 +0,0 @@
-grecaptcha.ready(function () {
-  grecaptcha.execute('6Lcmw-sZAAAAAJtZj1Q3abNdYCPvyYznCykMLdcT', {
-    action: 'contact'
-  }).then(function (token) {
-    var recaptchaResponse = document.getElementById('recaptchaResponse');
-    recaptchaResponse.value = token;
-  });
-});
