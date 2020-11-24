0bb0e02503fd3fd24580d96d6bad40cf563decb0
Aleksey Ponomarev <dazed39@mail.ru>
2020-11-24 23:02:24 +0200

    Fuck reCaptcha!

--- js/recaptcha.js
+++
@@ -1,8 +0,0 @@
-grecaptcha.ready(function () {
-  grecaptcha.execute('<?php echo SITE_KEY;?>', {
-    action: 'homepage'
-  }).then(function (token) {
-    console.log(token);
-    document.getElementById('g-recaptcha-response').value = token;
-  });
-});
