5dccbf8c5aa5c21e3ac887c8f998e0e240321f67
Aleksey Ponomarev <dazed39@mail.ru>
2020-12-01 23:44:47 +0200

    update ya.metrika

---
+++ js/ya.metrika.js
@@ -0,0 +1,27 @@
+// Yandex.Metrika counter
+
+let fired = false;
+
+window.addEventListener('scroll', () => {
+  if (fired === false) {
+    fired = true;
+
+    setTimeout(() => {
+      (function (m, e, t, r, i, k, a) {
+        m[i] = m[i] || function () {
+          (m[i].a = m[i].a || []).push(arguments)
+        };
+        m[i].l = 1 * new Date();
+        k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+      })
+      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+      ym(69732625, "init", {
+        clickmap: true,
+        trackLinks: true,
+        accurateTrackBounce: true,
+        webvisor: true
+      });
+    }, 1000)
+  }
+});
